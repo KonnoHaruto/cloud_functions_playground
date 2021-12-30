709b5c573b8e0b942aa975c836bb7804f713f301
Haruto Konno <qingdoujinye@gmail.com>
2021-12-30 15:25:17 +0900

    build(*): cloud_functionsを追加

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,10 +1,12 @@
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    FirebaseFunctions functions = FirebaseFunctions.instance;
     return Scaffold(
       appBar: AppBar(
         title: const Text('functions'),
